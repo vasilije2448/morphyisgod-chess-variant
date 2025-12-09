50bd32212a70f8f7b4cd6f5bf92b0276c27b3aae
vasilije2448 <73365189+vasilije2448@users.noreply.github.com>
2025-12-09 16:49:45 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+
+let boardAPI;
+
+function handleBoardCreated(api) {
+  boardAPI = api;
+}
+</script>
+
+<template>
+    <TheChessboard 
+      @board-created="handleBoardCreated"
+    />
+</template>
+
+<style scoped>
+</style>
